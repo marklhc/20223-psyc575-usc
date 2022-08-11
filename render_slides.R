c34f853d5bea0f148a97203f3e7b4b2cff96d862
Hok Chio (Mark) Lai <marklhc@gmail.com>
2022-08-11 16:17:27 -0700

    Add slides for Week 1 and regression refresher

---
+++ render_slides.R
@@ -0,0 +1,12 @@
+cat(getwd())
+# Convert Rmd slides to HTML and PDF
+for (f in dir(here::here("slides"), pattern = "*.Rmd", full.names = TRUE)) {
+  html_file <- xfun::with_ext(f, "html")
+  pdf_file <- xfun::with_ext(f, "pdf")
+  if (!file.exists(html_file) || file_test("-ot", html_file, f)) {
+    xfun::Rscript_call(rmarkdown::render, args = list(input = f))
+  }
+  if (!file.exists(pdf_file) || file_test("-ot", pdf_file, f)) {
+    pagedown::chrome_print(html_file)
+  }
+}
